39fa8b7f97ff82bb1e9e3f46c146a21de8e8538e
DstarL <lyons.dave@ymail.com>
2021-04-10 20:05:01 +0100

    Assignment 2 submission

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,44 @@
-## Put comments here that give an overall description of what your
-## functions do
+## ----------------------------------------------
+## File name: cachematrix.R
+## Author: David Lyons
+## Description: R Programming - Assignment 2
+## Date created: 2021-04-10
+## Date last modified: 2021-04-09
+## ----------------------------------------------
 
-## Write a short comment describing this function
-
+## Holds two cached variables
+## - x is a square invertible matrix 
+## - inv_mat is the inverse of x
+## Provides four internal functions to allow access
+## to cached variables or create new variables.
 makeCacheMatrix <- function(x = matrix()) {
-
+    inv_mat <- matrix()
+    set <- function(y = matrix()) {
+        x <<- y
+        inv_mat <<- matrix()
+    }
+    get <- function() x
+    setinv <- function(new_inv) inv_mat <<- new_inv
+    getinv <- function() inv_mat
+    list (set = set,
+          get = get,
+          setinv = setinv,
+          getinv = getinv)
 }
-
-
-## Write a short comment describing this function
-
+## Returns the inverse of the matrix held in
+## an instance of makeCacheMatrix. 
+## This will either be cached or recalculated 
+## for a new matrix 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    inv_mat <- x$getinv()
+    if (!is.na(inv_mat[1, 1])){
+        message("returning cached inverse matrix")
+        return(inv_mat)
+    }
+    message("generating inverse matrix")
+    new_mat <- x$get()
+    new_inv <- solve(new_mat, ...)
+    x$setinv(new_inv)
+    new_inv
 }
+
